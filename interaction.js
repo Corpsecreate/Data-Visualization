2b33d659cc54bd1e0b4e59448c600d2a6d2d9e6a
Corpsecreate <samsgwelling@gmail.com>
2016-06-07 15:48:08 +1000

    Add files via upload

---
+++ interaction.js
@@ -0,0 +1,24 @@
+
+
+function talk(words){
+	console.log(words)
+}
+alert('Hello');
+console.log('In Console');
+
+// document is the html file
+
+var Tri4 = document.getElementById("T4");
+
+function courage(){
+	console.log('power')
+}
+Tri4.addEventListener('click', function() 
+	{ 
+	console.log('Power!');
+	}
+)
+
+talk("hello, I am a cat")
+
+console.log(Tri4);
